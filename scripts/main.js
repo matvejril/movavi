a6032794fcebce09e5d28c818f1fdb62d7ab6a66
MatvejRil <matvej.ril@gmail.com>
2017-11-15 19:38:55 +0700

    correction js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
 window.onload = function() {
-    var btnToEasy = document.querySelectorAll(".to-easy");
-    var btnToFull = document.querySelectorAll(".to-full");
-    btnToEasy.forEach(function (item) {
-        item.addEventListener('click', toEasyMode);
-    });
-    btnToFull.forEach(function (item) {
-        item.addEventListener('click', toFullMode);
+    var changeStep = document.querySelectorAll(".to-easy, .to-full");
+    changeStep.forEach(function (item) {
+        item.addEventListener('click', changeStepFun);
     });
     btnsChange();
 };
@@ -14,29 +10,27 @@ window.onresize = function() {
     btnsChange();
 };
 
-function toEasyMode() {
+function changeStepFun(e) {
+    var btn = e.target;
+    var stepEasyModel = document.querySelector(".sm-guide-step_easy-mode");
+    var stepFullModel = document.querySelector(".sm-guide-step_full-mode");
     var btnCreateEasy = document.querySelector(".btn_create-easy");
     var btnCreateFull = document.querySelector(".btn_create-full");
-    var easyMode = document.querySelector(".sm-guide-step_easy-mode");
-    var fullMode = document.querySelector(".sm-guide-step_full-mode");
-    if (!btnCreateEasy.classList.contains('btn_create-active')) {
-        btnCreateEasy.classList.add('btn_create-active');
-        btnCreateFull.classList.remove('btn_create-active');
-        fullMode.style.display = 'none';
-        easyMode.style.display = 'block';
-    }
-}
 
-function toFullMode() {
-    var btnCreateEasy = document.querySelector(".btn_create-easy");
-    var btnCreateFull = document.querySelector(".btn_create-full");
-    var easyMode = document.querySelector(".sm-guide-step_easy-mode");
-    var fullMode = document.querySelector(".sm-guide-step_full-mode");
-    if (!btnCreateFull.classList.contains('btn_create-active')) {
-        btnCreateFull.classList.add('btn_create-active');
-        btnCreateEasy.classList.remove('btn_create-active');
-        easyMode.style.display = 'none';
-        fullMode.style.display = 'block';
+    if (btn.classList.contains('to-easy')) {
+        stepEasyModel.style.display = 'block';
+        stepFullModel.style.display = 'none';
+        if (!btn.classList.contains('btn_create-active')) {
+            btnCreateEasy.classList.add('btn_create-active');
+            btnCreateFull.classList.remove('btn_create-active');
+        }
+    } else {
+        stepEasyModel.style.display = 'none';
+        stepFullModel.style.display = 'block';
+        if (!btn.classList.contains('btn_create-active')) {
+            btnCreateEasy.classList.remove('btn_create-active');
+            btnCreateFull.classList.add('btn_create-active');
+        }
     }
 }
 
@@ -53,5 +47,3 @@ function btnsChange() {
         })
     }
 }
-
-
